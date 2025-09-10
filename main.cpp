8f7567bd3f914d1f96e7e8065af286723595c58a
250149 <250149@newuu.uz>
2025-09-10 12:56:50 +0500

    Week 3 Lab

---
+++ main.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+
+//                  Problem 11
+//
+// double doubleVariable1 = 1.13e3;
+// double doubleVariable2 = 411e-4;
+// int main() {
+//  cout << doubleVariable1 << endl;
+//  cout << doubleVariable2 << endl;
+// }
+
+
+//                  Problem 12
+// int intAdmitted = 241;
+// int intGroups = 11;
+// int intFirstGroup = intAdmitted/intGroups;
+// int intSecondGroup = intAdmitted%intGroups;
+// int main() {
+//     cout<<"In first group: "<< intFirstGroup<<endl;
+//     cout<<"In second group: "<< intSecondGroup<<endl;
+// }
+
+
+//                   Problem 13
+// int main() {
+//     double doubleArea,side;
+//
+//
+//     cout << "Enter the side length: ";
+//     cin >> side;
+//     doubleArea = ((3*sqrt(3))/2)*pow(side,2);
+//     cout << "The area of the hexagon is " << doubleArea << endl;
+//
+// }
+
+
+//                     Problem 14
+// int main() {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     int increment = num ++;
+//     int multiplication = num * 3;
+//     int subtraction = num - 14;
+//     int division = num / 4;
+//     int decrement = num --;
+//     int remainder = num % 9;
+//     num = remainder;
+//     cout << num << endl;
+// }
+
+
+//                  Problem 15
+// int main() {
+//     char a;
+//
+// }
+
+
+//                     Problem 16
+// int main() {}
+
+
+//                     Problem 17
+//  int main() {
+//      int a,b;
+//      cout<<"Enter the first number: ";
+//      cin>>a;
+//      cout<<"Enter the second number: ";
+//      cin>>b;
+//      cout<<(a==b)<<endl;
+//      return 0;
+// }
+
+
+//                     Problem 18
+// int main() {
+//     int a,b;
+//     cout<<"Enter the first number: ";
+//     cin>>a;
+//     cout<<"Enter the second number: ";
+//     cin>>b;
+//     cout<<(a<140)<<endl;
+//     cout<<(a<b)<<endl;
+//     return 0;
+// }
+
+
+//                      Problem 19
+// int main() {
+//     int a,b,c;
+//     cout<<"Enter the first number: ";
+//     cin>>a;
+//     cout<<"Enter the second number: ";
+//     cin>>b;
+//     cout<<"Enter the third number: ";
+//     cin>>c;
+//     cout<<((a>b)&&(a>c));
+//     return 0;
+// }
+
+
+//                        Problem 20
+// int main() {
+//     int a, b, c, d, e;
+//     cout << "Please enter the first number: " << endl;
+//     cin >> a;
+//     cout << "Please enter the second number: " << endl;
+//     cin >> b;
+//     cout << "Please enter the third number: " << endl;
+//     cin >> c;
+//     cout << "Please enter the fourth number: " << endl;
+//     cin >> d;
+//     cout << "Please enter the fifth number: " << endl;
+//     cin >> e;
+//     cout <<((a>=0)||(b>=0)||(c>=0)||(d>=0)||(e>=0)) << endl;
+//     return 0;
+// }
+
+
+//                    Problem 21
+// int main() {
+//     int a,b;
+//     cout<<"Enter the first number: ";
+//     cin>>a;
+//     cout<<"Enter the second number: ";
+//     cin>>b;
+//     cout<<((a+b)>30&&(a*b)>30);
+//     cout<<((a+(b-10))>30&&(a*(b-10))>30);
+//     cout<<((a>30)&&(b>30));
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
